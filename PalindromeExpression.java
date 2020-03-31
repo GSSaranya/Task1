d401ff9c0844d587abbc0bfe6e86eb5b898dbd0b
GSSaranya <58139546+GSSaranya@users.noreply.github.com>
2020-03-31 12:22:52 +0530

    Add files via upload

---
+++ PalindromeExpression.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+class PalindromeExpression
+{
+	public static void main(String args[])
+{
+			Scanner sc=new Scanner(System.in);
+			String  str;
+			System.out.println("Enter the String:");
+			str=sc.nextLine();
+			str=str.toLowerCase();
+			 str=str.replaceAll("(\\W)","");
+			
+			System.out.println(str);
+			int length=str.length();
+ 		for( int i=0;i<=length/2;i++)
+		{
+		
+		if(str.charAt(i)!=str.charAt((str.length())-i-1))
+		{
+		System.out.println("NOT A PALINDROME");
+		}
+		}
+		System.out.println(" A PALINDROME");
+		
+}
+}
